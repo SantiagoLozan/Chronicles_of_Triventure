bc3ae309f3b6d750d2b7a91754cd6bcab6f364ac
Santiago Lozan <santiagolozan73@gmail.com>
2023-09-05 18:40:31 -0300

    agregado de assets para scenenav

--- src/scenes/HelloWorldScene.js
+++ src/scenes/HelloWorldScene.js
@@ -1,6 +1,5 @@
 import Phaser from "phaser";
 
-
 // Manejador de eventos centralizados para comunicacion de componentes
 
 // Importacion
@@ -21,11 +20,13 @@ export default class HelloWorldScene extends Phaser.Scene {
 
   create() {
     this.add.image(400, 300, "sky");
-
-    
+    this.add.text(70, 100, "Chronicles of Triventure");
 
     // launch UI scene
     this.scene.launch("ui");
-
+    this.botonAtras = this.add.image(75, 525, "botonAtras").setScale(0.2);
+    this.botonInfo = this.add.image(325, 525, "botonInfo").setScale(0.2);
+    this.botonMenu = this.add.image(150, 350, "botonMenu").setScale(0.6);
     }
+    
   }
--- src/scenes/Preload.js
+++ src/scenes/Preload.js
@@ -10,17 +10,25 @@ export default class Precarga extends Phaser.Scene {
     this.load.image("sky", "assets/skies/space3.png");
     this.load.image("botonMenu", "assets/images/botonMenu.png");
     this.load.image("botonAtras", "assets/images/botonAtras.png");
+    this.load.image("botonInfo", "assets/images/botonInfo.png");
+    this.load.image("enter", "assets/images/teclaEnter.png");
+    this.enter = this.input.keyboard.addKey(
+        Phaser.Input.Keyboard.KeyCodes.ENTER
+      );
     }
-   
+
     create(){
-        this.add.image(400, 300, "sky");
-        this.botonMenu = this.add.image(395, 525, "botonmenu");
+        this.botonEnter = this.add.image(175, 525, "enter").setScale(0.5);
     }
 
     update(){
-        this.botonMenu
-        .setInteractive({ useHandCursor: true })
-        // @ts-ignore
-        .on("pointerdown");
-    }
- }
+    this.botonEnter
+      .setInteractive({ useHandCursor: true })
+      .on("pointerdown", () => this.arranqueMenu());
+  }
+
+  arranqueMenu() {
+    this.scene.start("hello-world");
+  }
+}
+ 
